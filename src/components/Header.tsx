e40678dc28a070ce698e7e9d8b0ec0ca03e56913
macOS <macos@danilo.local>
2024-07-24 11:02:41 -0300

    estilizacao do banner completa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import logo from '../assets/logo.png'
+
 export function Header() {
   return(
-    <h1>Header</h1>
+    <header className="w-full px-40 h-28 fixed top-0 bg-background">
+      <div className="h-full flex items-center justify-between">
+        <div>
+          <img className="w-20 h-10" src={logo} alt="coffee delivery logo" />
+        </div>
+
+        <div className="flex items-center gap-3">
+          <div className="flex gap-1 items-center p-2 bg-purple-light rounded-md text-purple-dark">
+            <MapPin size={24} color="#8047F8" weight="fill"/>
+            <span className="font-roboto">Porto Alegre, RS</span>
+          </div>
+
+          <div className="p-2 bg-yellow-light rounded-md">
+            <ShoppingCart size={24} color="#C47F17" weight="fill" />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
